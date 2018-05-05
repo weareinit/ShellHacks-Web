30487f5db1d98f34a1f3b9783c67abe5817b9d16
Gabriel Fajardo <gfajardo95@gmail.com>
2018-05-05 14:21:19 -0400

    create simple banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './banner.css';
+
+export default function Banner() {
+  return (
+    <section className='banner'>
+      <p>ShellHacks 2018</p>
+    </section>
+  );
+}
