8c05d166250a2a151b46d2c83fc390893a44ddde
Gabriel Fajardo <gfajardo95@gmail.com>
2018-05-06 18:55:37 -0400

    add navbar and footer to html and grid layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import Banner from '../Banner/Banner';
+import Footer from '../Footer/Footer';
+import Nav from '../Nav/Nav';
+
 import './app.css';
 
 export default function App() {
   return (
     <div className='app'>
+      <Nav />
       <Banner />
+      <Footer />
     </div>
   );
 }
