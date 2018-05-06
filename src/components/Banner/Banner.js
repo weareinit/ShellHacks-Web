fe72cbcc1bce2a319919a9a631dc6289988cc022
Gabriel Fajardo <gfajardo95@gmail.com>
2018-05-06 18:52:07 -0400

    modify style and remove <p> element

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,9 +3,5 @@ import React from 'react';
 import './banner.css';
 
 export default function Banner() {
-  return (
-    <section className='banner'>
-      <p>ShellHacks 2018</p>
-    </section>
-  );
+  return <section className="banner" />;
 }
