ab338e80747ca257be8048a0e253800dc47ce6a4
Gabriel Fajardo <gfajardo95@gmail.com>
2018-05-06 18:53:21 -0400

    create navbar

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './nav.css';
+
+export default function Nav() {
+  return (
+    <div className="navbar">
+      <a>About</a>
+      <a>Schedule</a>
+      <a>Sponsors</a>
+    </div>
+  );
+}
