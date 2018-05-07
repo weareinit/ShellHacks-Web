9d3d7e80f5de419a433feca0099a502850cf29bc
Tommy2016x <Carrascalx@gmail.com>
2018-05-07 17:39:02 -0400

    fixed formatting

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,11 +4,9 @@ import './banner.css';
 
 export default function Banner() {
   return (
-  	
-  	<section className="banner">
-  	<h1 className="title"> ShellHacks Babyyy!!! </h1>
-  	</section>
-  	
-  	
-  	);
+
+<section className="banner">
+<h1 className="title"> ShellHacks Babyyy!!! </h1>
+</section>
+  );
 }
