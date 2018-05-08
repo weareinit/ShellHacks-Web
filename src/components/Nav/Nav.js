b13f583d68086ccf3b365d77dcb20dfa8e1fe0e4
Gabriel Fajardo <gfajardo95@gmail.com>
2018-05-08 10:16:11 -0400

    fix class name attribute

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,7 +5,7 @@ import './nav.css';
 export default function Nav() {
   return (
     <ul className="navbar">
-      <li href="" class="shellhacks-nav-logo">ShellHacks</li>
+      <li href="" className="shellhacks-nav-logo">ShellHacks</li>
       <li href="" className="navbar-item">About</li>
       <li href="" className="navbar-item">Schedule</li>
       <li href="" className="navbar-item">Sponsors</li>
