fe4bbc8eca74b34a536b0e14aa96f7a8d7684c1f
Gabriel Fajardo <gfajardo95@gmail.com>
2018-05-14 15:38:11 -0400

    implement drop down menu for responsive navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,14 +1,42 @@
 import React from 'react';
+import WindowSizeListener from 'react-window-size-listener';
 
 import './nav.css';
 
 export default function Nav() {
+  function toggleBurger() {
+    const menuItems = document.querySelector('.navbar');
+    if (menuItems.style.display === 'flex') {
+      menuItems.style.display = 'none';
+    } else {
+      menuItems.style.display = 'flex';
+    }
+  }
+
+  function handleResize(windowSize) {
+    const menu = document.querySelector('.navbar');
+    if (windowSize.windowWidth >= 480) {
+      if (menu.style.display === 'none') {
+        menu.style.display = 'flex';
+      }
+    } else if (windowSize.windowWidth < 480) {
+      if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+      }
+    }
+  }
+
   return (
+    <nav>
+    <WindowSizeListener onResize={handleResize}></WindowSizeListener>
+    <a href="" className="logo">ShellHacks</a>
+    <button className='burger-button' onClick={toggleBurger}><i className='fa fa-bars fa-2x'></i></button>
     <ul className="navbar">
-      <li href="" className="shellhacks-nav-logo">ShellHacks</li>
-      <li href="" className="navbar-item">About</li>
-      <li href="" className="navbar-item">Schedule</li>
-      <li href="" className="navbar-item">Sponsors</li>
+      <li className='navbar-item'><a href="#">About</a></li>
+      <li className='navbar-item'><a href="#">Schedule</a></li>
+      <li className='navbar-item'><a href="#">Sponsors</a></li>
+      <button className='menu-close-button' onClick={toggleBurger}>X</button>
     </ul>
+    </nav>
   );
 }
