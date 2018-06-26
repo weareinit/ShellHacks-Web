e4906020cf9e02af1fcec0bfbc0df7ae7aa89a81
Gabriel Fajardo <gfajardo95@gmail.com>
2018-06-26 17:50:38 -0600

    add shellhacks logo and modify colors

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -8,7 +8,8 @@ export default function Banner() {
   return (
     <section className='banner'>
     <div className='banner-text'>
-      <h1>ShellHacks 2018</h1>
+      <img src='/src/assets/ShellHacks_WordMark_White@4x.png'></img>
+      {/* <h1>ShellHacks 2018</h1> */}
       <p>September 21 | Florida International University</p>
       <button>< RegisterLink/></button>
     </div>
