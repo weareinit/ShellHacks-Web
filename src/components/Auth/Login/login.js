318a13afc29f5f94b58bc9324faaaa6d7bd7f189
Gabriel Fajardo <gfajardo95@gmail.com>
2018-07-16 20:14:39 -0600

    modify layout to accommodate the Nav component

--- src/components/Auth/Login/login.js
+++ src/components/Auth/Login/login.js
@@ -1,21 +1,25 @@
 import React from 'react';
+
+import Nav from '../../Nav/Nav';
 import './login.css';
 
 export default function login() {
   return (
-<div>
-  <h1> Login </h1><br />
-    <div className="form">
-    <form>
-      <h2> Email </h2>
-      <input type = "textfield" />
-      <h2> Password </h2>
-      <input className="password" type = "textfield" /><br/>
-      <button className="btn btnforgot" >Forgot Password?</button>
-      <button className="btn btnlogin">Login</button>
-    </form>
+  <div className='login'>
+    <Nav />
+    <div className='login-container'>
+    <h1> Login </h1><br />
+      <div className="form">
+      <form>
+        <h2> Email </h2>
+        <input type = "textfield" />
+        <h2> Password </h2>
+        <input className="password" type = "textfield" /><br/>
+        <button className="btn btnforgot" >Forgot Password?</button>
+        <button className="btn btnlogin">Login</button>
+      </form>
+      </div>
     </div>
-</div>
+  </div>
   );
 }
-
