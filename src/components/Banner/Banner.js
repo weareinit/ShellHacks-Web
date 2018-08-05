5ccf079dbea2b88e409fc9bb88008235ce244bff
Gabriel Fajardo <gfajardo95@gmail.com>
2018-08-04 20:30:55 -0600

    add home page anchor link when on the main page

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -9,7 +9,7 @@ import './banner.css';
 
 export default function Banner() {
   return (
-    <section className='banner'>
+    <section className='banner' id='banner'>
     <div className='banner-text'>
       <img src='/src/assets/ShellHacks_WordMark_White@4x.png'></img>
       {/* <h1>ShellHacks 2018</h1> */}
