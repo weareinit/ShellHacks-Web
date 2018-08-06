e927962765f27a07a92e8feea311eb4757056a9f
Tommy2016x <Carrascalx@gmail.com>
2018-08-06 16:23:48 -0400

    updated the schedule section and added some elements to FAQ section as well

--- src/components/Faq/FaqElem/FaqElem.js
+++ src/components/Faq/FaqElem/FaqElem.js
@@ -6,6 +6,9 @@ const FaqElem = props => (
   <div className='faq-elem'>
     <h3>{props.question}</h3>
     <p>{props.answer}</p>
+    <div className="img">
+    <img width="40" height="40"src='/src/assets/ShellHacks_Logo_White@4x.png' />
+    </div>
   </div>
 );
 
