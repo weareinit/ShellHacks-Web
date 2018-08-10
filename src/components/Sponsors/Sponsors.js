752774bc5a8caa042b03168f65b66ac52595cdfa
Tommy2016x <Carrascalx@gmail.com>
2018-08-09 23:05:05 -0400

    added functions to schedule section to load images for sponsors and partners from object, needs API fetch implementation and styling

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -1,23 +1,145 @@
-import React from 'react';
+/*eslint-disable */
+
+import React,{Component} from 'react';
 
 import './sponsors.css';
 
-export default function Sponsors() {
+let data = [
+  {
+      "name": "Microsoft",
+      "tier": "conch",
+      "image": "http://thetriangle.news/wp-content/uploads/2017/10/Microsoft-The-Triangle.jpg",
+      "url": "http://microsoft.com"
+  },
+  {
+    "name": "Microsoft",
+    "tier": "cone",
+    "image": "http://thetriangle.news/wp-content/uploads/2017/10/Microsoft-The-Triangle.jpg",
+    "url": "http://microsoft.com"
+},{
+  "name": "Microsoft",
+  "tier": "cone",
+  "image": "http://thetriangle.news/wp-content/uploads/2017/10/Microsoft-The-Triangle.jpg",
+  "url": "http://microsoft.com"
+},
+{
+  "name": "Microsoft",
+  "tier": "cone",
+  "image": "http://thetriangle.news/wp-content/uploads/2017/10/Microsoft-The-Triangle.jpg",
+  "url": "http://microsoft.com"
+},
+{
+  "name": "Microsoft",
+  "tier": "scallop",
+  "image": "http://thetriangle.news/wp-content/uploads/2017/10/Microsoft-The-Triangle.jpg",
+  "url": "http://microsoft.com"
+},
+{
+  "name": "Microsoft",
+  "tier": "scallop",
+  "image": "http://thetriangle.news/wp-content/uploads/2017/10/Microsoft-The-Triangle.jpg",
+  "url": "http://microsoft.com"
+},
+{
+  "name": "Microsoft",
+  "tier": "scallop",
+  "image": "http://thetriangle.news/wp-content/uploads/2017/10/Microsoft-The-Triangle.jpg",
+  "url": "http://microsoft.com"
+},{
+  "name": "Microsoft",
+  "tier": "scallop",
+  "image": "http://thetriangle.news/wp-content/uploads/2017/10/Microsoft-The-Triangle.jpg",
+  "url": "http://microsoft.com"
+},{
+  "name": "Microsoft",
+  "tier": "sand-dollar",
+  "image": "http://thetriangle.news/wp-content/uploads/2017/10/Microsoft-The-Triangle.jpg",
+  "url": "http://microsoft.com"
+},{
+  "name": "Microsoft",
+  "tier": "sand-dollar",
+  "image": "http://thetriangle.news/wp-content/uploads/2017/10/Microsoft-The-Triangle.jpg",
+  "url": "http://microsoft.com"
+},
+{
+  "name": "Microsoft",
+  "tier": "partner",
+  "image": "http://thetriangle.news/wp-content/uploads/2017/10/Microsoft-The-Triangle.jpg",
+  "url": "http://microsoft.com"
+},{
+  "name": "Microsoft",
+  "tier": "partner",
+  "image": "http://thetriangle.news/wp-content/uploads/2017/10/Microsoft-The-Triangle.jpg",
+  "url": "http://microsoft.com"
+},
+{
+  "name": "Microsoft",
+  "tier": "partner",
+  "image": "http://thetriangle.news/wp-content/uploads/2017/10/Microsoft-The-Triangle.jpg",
+  "url": "http://microsoft.com"
+}
+];
+
+class Sponsors extends Component {
+    constructor(){
+      super();
+      this.loadImages = this.loadImages.bind(this);
+      this.loadPartners = this.loadPartners.bind(this);
+    }
+
+    loadImages (){
+      let shell;
+      let image;
+      let url;
+      let sponsors = []
+      for(let i = 0; i< data.length; i++){
+        if(data[i].tier != 'partner'){
+        shell = data[i].tier;
+        image = data[i].image;
+        url = data[i].url;
+        sponsors.push(
+          <div><a href = {url}> <img src = {image} className = {shell} /> </a> </div>
+        )
+      }
+    }
+
+      return sponsors;
+    }
+
+    loadPartners (){
+      let shell;
+      let image;
+      let url;
+      let partners = []
+      for(let i = 0; i< data.length; i++){
+        if(data[i].tier == 'partner'){
+        shell = data[i].tier;
+        image = data[i].image;
+        url = data[i].url;
+        partners.push(
+          <div><a href = {url}> <img src = {image} className = {shell} /> </a> </div>
+        )
+      }
+    }
+
+      return partners;
+    }
+
+    render(){
+      
+    
   return (
     <div className='sponsors-container' id='sponsors'>
     <div className='sponsors-content'>
-      <h1>Sponsors</h1>
-      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Cras auctor tempus metus, non dignissim enim blandit a. Aenean
-      sollicitudin turpis et enim vehicula eleifend. Sed non ipsum leo.
-      Quisque id lectus sed arcu bibendum tempus non sit amet augue.
-      Curabitur leo ante, lacinia vel lobortis ac, luctus vel neque.
-      Maecenas gravida sed felis eget eleifend. Duis sem orci, aliquet
-      at tortor quis, convallis suscipit erat. Quisque sed rutrum neque.
-      Phasellus eget nisi dignissim, dapibus arcu non, pulvinar est.
-      Donec tellus lacus, feugiat non ante id, elementum vehicula nisi.
-      Vestibulum magna orci, scelerisque id dapibus porta, varius ac odio.</p>
+    <h1> Sponsors </h1> 
+      {this.loadImages()}
+      <h1> Partners </h1>
+      {this.loadPartners()}
     </div>
+
     </div>
   );
 }
+}
+
+export default Sponsors;
